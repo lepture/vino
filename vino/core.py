39fd26caa233a4d0af984945d7e8e4315ec59e92
lepture <lepture@me.com>
2012-07-18 21:57:52 +0800

    prepare connection for core

--- vino/core.py
+++ vino/core.py
@@ -5,8 +5,8 @@ class Vino(object):
     last_query = None
     last_query_time = None
 
-    def __init__(self, db):
-        self._database = _connect(db)
+    def __init__(self, db, **kwargs):
+        self._db = _connect(db, **kwargs)
 
     def table(self, name):
         pass
@@ -18,5 +18,38 @@ class Vino(object):
         pass
 
 
-def _connect(db):
-    pass
+class _Database(object):
+    engine = None
+    host = None
+    port = None
+    user = None
+    passwd = None
+    dbname = None
+
+    def __init__(self, db):
+        # mysql://user:pass@localhost:port/db
+        self.engine, host = db.splite('://')
+        if '@' not in host:
+            self.host = host
+            return
+
+        self.user, host = host.split('@')
+        if ':' in self.user:
+            self.user, self.passwd = self.user.split(':')
+
+        self.host, self.dbname = host.split('/')
+        if ':' in self.host:
+            self.host, self.port = self.host.split(':')
+
+
+def _connect(db, **kwargs):
+    d = _Database(db)
+    if d.engine == 'sqlite':
+        from .engines._sqlite import Connection
+        return Connection(d.host)
+    elif d.engine == 'mysqlite':
+        from .engines._mysql import Connection
+        max_idle_time = kwargs.pop('max_idle_time', 7 * 3600)
+        return Connection(d.host, d.dbname, d.user, d.passwd, max_idle_time)
+    else:
+        raise ValueError("Doesn't support %s yet" % d.engine)
